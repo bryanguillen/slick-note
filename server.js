3289ea3736bf2e10bed1fbba539e348e4dea1883
Bryan <artificialknowledge217@gmail.com>
2017-04-11 10:18:45 -0400

    add notesRouter

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const userHomeRouter = require('./userHomeRouter');
+const notesRouter = require('./notesRouter');
 const app = express();
 
 //serving static files
@@ -15,6 +16,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan('common'));
 
 app.use('/me', userHomeRouter);
+app.use('/note', notesRouter);
 
 //** remember to add mongoose promise as a global promise. 
 
