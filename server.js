39bb8586c6a9814fae743bce8000ba3a38f185a7
Bryan <artificialknowledge217@gmail.com>
2017-04-10 22:18:00 -0400

    add closeServer function

--- server.js
+++ server.js
@@ -7,12 +7,24 @@ app.use(express.static('public'));
 
 //** remember to add mongoose promise as a global promise. 
 
-//server listening
+//start and kill server code below
+//TODO: ADD CONFIG VALUES LATER, ESPECIALLY MONGOOSE VALS LATER
 function runServer() {
 	return new Promise((resolve, reject) => { 
-		//ADD CONFIG VALUES LATER
 		server = app.listen(process.env.PORT || 8080, function() {
 			console.log('app is now listening on port 8080!')
+			resolve();
+		});
+	});
+}
+
+function closeServer() {
+	return new Promise((resolve, reject) => {
+		console.log('Killing the server now!')
+		server.close(err => {
+			if(err) {
+				return reject(err);
+			}
 			resolve();
 		});
 	});
