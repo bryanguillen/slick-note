918f94e895769d2119db0d63e3d3641c14e5f585
Bryan <artificialknowledge217@gmail.com>
2017-04-10 22:15:15 -0400

    add runServer promise

--- server.js
+++ server.js
@@ -5,7 +5,21 @@ const app = express();
 //serving static files
 app.use(express.static('public'));
 
+//** remember to add mongoose promise as a global promise. 
+
 //server listening
-app.listen(process.env.PORT || 8080, function() {
-	console.log('app is now listening on port 8080!')
-});
+function runServer() {
+	return new Promise((resolve, reject) => { 
+		//ADD CONFIG VALUES LATER
+		server = app.listen(process.env.PORT || 8080, function() {
+			console.log('app is now listening on port 8080!')
+			resolve();
+		});
+	});
+}
+
+if (require.main === module) {
+  runServer().catch(err => console.error(err));
+};
+
+module.exports = {app, runServer};
