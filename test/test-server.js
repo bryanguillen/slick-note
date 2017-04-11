4f2f5933a18c7cd4029dc8dba5b2efab26ac635e
Bryan <artificialknowledge217@gmail.com>
2017-04-10 23:04:57 -0400

    first commit for test

--- test/test-server.js
+++ test/test-server.js
@@ -2,10 +2,30 @@
 const chai = require('chai');
 const chaiHTTP = require('chai-http');
 const faker = require('faker');
-const {app} = require('../server');
+const {app, runServer, closeServer} = require('../server');
 const should = chai.should();
 const mongoose = require('mongoose');
 
 //allowing for http integration testing
 chai.use(chaiHTTP);
 
+describe('ENDPOINTS TEST', function() {
+	before(function() {
+		return runServer();
+	})
+
+	after(function() {
+		return closeServer();
+	})
+
+	describe('Get the index static file', function() {
+		it('should render static index.html', function() {
+			return chai.request(app)
+				.get('/')
+				.then(function(res) {
+					res.should.have.status(200);
+				})
+		});
+	})
+
+})
