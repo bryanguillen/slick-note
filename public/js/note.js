6f860f4e812984234a581350b1d6108e46b1af0e
Bryan <artificialknowledge217@gmail.com>
2017-04-11 14:08:27 -0400

    create show section listener

--- public/js/note.js
+++ public/js/note.js
@@ -9,12 +9,18 @@ var state = {};
 var userNotes = {
 	notes: [
 		{
-			header: "The Time Value Of Money",
-			content: "The time value of money in the chapter\
-						states that what is more valuable than \
-						money is time. You can run out of money a hundred\
-						times and still recover from that.\
-						But if you lose time, you cannot recover from that."
+			title: "FINANCE THE TIME VALUE OF MONEY",
+			subTitle: "FOR FINANCE 101",
+			sections: [
+					{
+						header: "The Time Value Of Money",
+						content: "The time value of money in the chapter " +
+						"states that what is more valuable than " +
+						"money is time. You can run out of money a hundred " +
+						"times and still recover from that. " +
+						"But if you lose time, you cannot recover from that."
+					}
+			]
 		}
 	]
 }
@@ -25,21 +31,42 @@ function getCurrentNote() {
 	var note = userNotes.notes[0];
 	function createNoteHTML(note) {
 		var html = '<div class="header">' + 
-						note.header + 
+						note.sections[0].header + 
 					'</div>' + 
 					'<div class="content">' +
-						note.content + 
+						note.sections[0].content + 
 					'</div>'
 		return displayCurrentNote(html);
 	}
 	return createNoteHTML(note);
 }
+
+function getSections() {
+	//get the sections here to display on the side bar of current section
+	//these are going to be clickable sections that the user can use to navigate
+	var title = '<div>' + userNotes.notes[0].title + '</div>' + 
+				'<div>' + userNotes.notes[0].subTitle + '</div>'
+
+	var noteSections =  '<div>' + userNotes.notes[0].sections[0].header + '</div>'
+	$('div.section-container').html(title + noteSections); 
+}
+
 //DOM MANIPULATION
 function displayCurrentNote(currentNote) {
 	$('div.note').html(currentNote);
 }
 
 //EVENT LISTENERS
+function showSection() {
+	$('main').on('click', '.show-section-button', function(event) {
+		event.preventDefault();
+		$(this).addClass('js-hide-section-button');
+		$('div.section-container').addClass('js-inline').removeClass('js-hide-sections');
+		$('div.note-container').addClass('js-inline');
+		getSections();
+	})
+}
+
 function editCurrentNote() {
 	$('main').on('click', '.note', function(event) {
 		event.preventDefault();
@@ -58,4 +85,5 @@ function editCurrentNote() {
 $(function() {
 	getCurrentNote();
 	editCurrentNote();
+	showSection();
 })
