06f50bcf5b2729471d0f2519eb49abe6913ea554
Bryan <artificialknowledge217@gmail.com>
2017-04-11 20:53:39 -0400

    add values

--- config.js
+++ config.js
@@ -1,1 +1,6 @@
-//CONFIG VALUES WILL GO HERE A LITTLE LATER
+exports.DATABASE_URL = process.env.DATABASE_URL ||
+						global.DATABASE_URL ||
+						'mongodb://localhost/notes-app';
+exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL ||
+							'mongodb://localhost/test-notes-app';
+exports.PORT = process.env.PORT || 8080; 
