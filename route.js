ba10a0cb543dc847ddf37c95cdfb480a18b92dcf
Bryan <artificialknowledge217@gmail.com>
2017-04-12 12:43:47 -0400

    add .json for json file in ajax request

--- route.js
+++ route.js
@@ -3,20 +3,38 @@ const router = express.Router();
 const {app, runServer, closeServer} = require('./server.js');
 const {User} = require('./usersModel');
 const {Note} = require('./notesModel');
-const path = require('path');
 
 //more dependencies and imports
 const bodyParser = require('body-parser');
 const jsonParser = bodyParser.json();
 
 
-router.get('/:username', (req, res) => {
+router.get('/:username.json', (req, res) => {
 	User 
 		.findOne({"username": req.params.username})
 		.exec()
-		.then(user => res.json(user.apiRepr()));
-	//res.sendFile(path.join(__dirname + '/public/home.html'));
+		.then(user => res
+			.json(user.apiRepr())
+			)
+		.catch(err => {
+			console.log(err);
+			res.status(500).json({errorMsg: "internal server error"});
+		})
 });
+
+router.get('/:username', (req, res) => {
+	User
+		.findOne({"username": req.params.username})
+		.exec()
+		.then(user => res
+			.cookie('username', req.params.username)
+			.sendFile(__dirname + '/public/home.html')
+			)
+		.catch(err => {
+			console.log(err);
+			res.status(500).json({errorMsg: "internal server error"});
+		})
+})
 
 router.get('/:id', (req, res) => {
 	res.sendFile(path.join(__dirname + '/public/note.html'));
