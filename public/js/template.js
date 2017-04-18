937cea8a0a912a1f3817f86850eef702bf32e82c
Bryan <artificialknowledge217@gmail.com>
2017-04-18 00:04:43 -0400

    add note template

--- public/js/template.js
+++ public/js/template.js
@@ -14,3 +14,21 @@ function getNewTemplate() {
 			</div>
 			<button class="show-section-button"> >> </button>`
 }
+
+function getNoteTemplate() {
+	//template for the actual note taking of the application
+	//couldn't think of a better name for now.!
+	return `<div class="section-container js-hide-sections">
+			</div>
+			<div class="note-container">
+				<div class="note-options">
+					<span>New Section</span>
+					<span>New SubSection</span>
+				</div>
+				<div class="note">
+				</div>
+				<textarea class="note js-edit-note"></textarea>
+				<button class="save-note js-save-note">Save</button>
+			</div>
+			`
+}
