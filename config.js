10368b6dee80439d722a696e4cc5f4b614d2a4e2
Bryan <artificialknowledge217@gmail.com>
2017-04-17 23:10:25 -0400

    add mlab test db

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 exports.DATABASE_URL = process.env.DATABASE_URL ||
 						global.DATABASE_URL ||
 						'mongodb://localhost/notes-app';
-exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL ||
+exports.TEST_DATABASE_URL = 'mongodb://bryan:pudding34@ds161960.mlab.com:61960/test_database' ||
 							'mongodb://localhost/test-notes-app';
 exports.PORT = process.env.PORT || 8080; 
