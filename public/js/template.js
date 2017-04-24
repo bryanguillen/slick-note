b82f4592bee720cad0fb5995c8530190e0a05ab7
Bryan <artificialknowledge217@gmail.com>
2017-04-24 16:04:45 -0400

    add published note template

--- public/js/template.js
+++ public/js/template.js
@@ -44,27 +44,33 @@ function getNoteTemplate(title, subtitle, noteId) {
 
 function getPublishedNoteTemplate(title, subtitle, notes, noteId) {
 	//return to this and see if better way. 
-	return `<div class="title-container">
-				<div class="title">
-					<span class="title-text">  ${title} </span> 
-					<span class="subtitle-text"> ${subtitle} </span> 
-				</div>
-				<div class="edit-title">  
-					<label>title</label>
-					<input type="text" name="title" required />
-					<label>subtitle</label>
-					<input type="text" name="subtitle" required />
-					<button class="update-titles">Update</button> 	
-				</div> 
-			</div>
-			<div class="note-container">
-				<!-- notes added so user could read their notes -->
-				<div class="note">${notes}</div>
-				<!-- hide-edit-note added to render just note so user could read without edit -->
-				<div class="editing-note-container hide-edit-note">
-					<textarea class="edit-note"></textarea>
-					<button class="save-note">Save</button>
-					<div class="note-id">${noteId}</div>
-				</div>
-			</div>`	
+	return `<div class="row">
+                <div class="col-12 title-container">
+                    <div class="title">
+                        <span class="title-text">${title}</span> 
+                        <span class="subtitle-text">${subtitle}</span> 
+                    </div>
+                    <div class="edit-title">  
+                        <div class="edit-title-container">
+                            <label class="update-title-label">title</label>
+                            <input type="text" name="title" class="update-title-field" required />
+                        </div>
+                        <div class="edit-subtitle-container">
+                            <label class="update-subtitle-label">subtitle</label>
+                            <input type="text" name="subtitle" class="update-subtitle-field" required />   
+                        </div>
+                        <button class="update-titles">Update</button>
+                    </div> 
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-12 note-container">
+                    <div class="note">${notes}</div>
+                    <div class="editing-note-container hide-edit-note">
+                        <textarea class="edit-note"></textarea>
+                        <div class="save-button-container"><button class="save-note">Save</button></div>
+                        <div class="note-id">${noteId}</div>
+                    </div>
+                </div>
+            </div>`	
 }
