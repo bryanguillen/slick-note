74909c169d1d4c1ed8ee6344b2c2b5e138226ceb
Bryan <artificialknowledge217@gmail.com>
2017-04-25 11:52:49 -0400

    add real dbs

--- config.js
+++ config.js
@@ -1,10 +1,5 @@
-exports.DATABASE_URL = process.env.DATABASE_URL ||
-						global.DATABASE_URL ||
-						'mongodb://localhost/notes-app';
+exports.DATABASE_URL = 'mongodb://bryan:pudding34@ds159670.mlab.com:59670/notes-app' || 'mongodb://localhost/notes-app'; 
 
-exports.TEST_DATABASE_URL = 'mongodb://localhost/test-notes-app';
-
-exports.TEST_USER_DATABASE_URL = 'mongodb://bryan:pudding34@ds157459.mlab.com:57459/test_user_database' ||
-								'mongodb://localhost/test-user-database';
+exports.TEST_DATABASE_URL = 'mongodb://bryan:pudding34@ds161960.mlab.com:61960/test_database' || 'mongodb://localhost/test-notes-app';
 
 exports.PORT = process.env.PORT || 8080; 
