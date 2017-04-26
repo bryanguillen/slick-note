e833a96e0eee71cf7b9e67cb25444755b0705ffb
Bryan <artificialknowledge217@gmail.com>
2017-04-25 20:14:24 -0400

    change values on this

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-exports.DATABASE_URL =  'mongodb://localhost/notes-app' || process.env.DATABASE_URL; 
+exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/notes-app'; 
 
 exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/test-notes-app';
 
