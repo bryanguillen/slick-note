6baa8d57a7a9bce17a5e129388232b9e6edb5f8f
Bryan <artificialknowledge217@gmail.com>
2017-04-26 22:37:21 -0400

    add test database url via process.env

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/notes-app'; 
 
-exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/test-notes-app';
+exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL;
 
 exports.PORT = process.env.PORT || 8080; 
