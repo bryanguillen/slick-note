fbb522ffc02214d3b4ee781ce7ed5cd1053b4463
Bryan <artificialknowledge217@gmail.com>
2017-05-05 22:22:03 -0400

    add demo material

--- public/js/unauthorizedClient.js
+++ public/js/unauthorizedClient.js
@@ -1,5 +1,254 @@
+//State MGMT For Demo. 
+state = {
+	users: [
+	{
+		id: 200,
+		username: 'demoUser123',
+		userNotes: [
+			{
+				id: 1,
+				title: 'Hello demoUser123',
+				content: 'This is a ``demo`` feel free to read me, or edit me. Whatever you like'
+			}, {
+				id: 2,
+				title: 'Learning Software Dev',
+				content: 'Thus far, it has been an enjoyable journey. I mean ' +
+						'It has been full of challenges but with ``the help of my mentor`` and supporting staff ' +
+						'the journey has been a life changing experience.'
+			}
+		]
+	}
+	],
+	notes: [
+	{
+		id: 1,
+		user: 200,
+		title: 'Hello World',
+		content: 'This is a ``demo`` feel free to read me, or edit me. Whatever you like.'
+	},
+	{
+		id: 2,
+		user: 200,
+		title: 'Learning Software Development',
+		content: 'Thus far, it has been an enjoyable journey. I mean it has been full of challenges but with  ``the help of my mentor`` and supporting staff '
+	}
+	],
+
+	dbIdSetter: 2
+}
+
 //Template to avoid calls to the server
 //just handling unauthorized people in the client.
+//DOM MANIPULATION
+function renderLogout(data) {
+	$('body').html(data);
+}
+
+function renderNewNoteTemp() {
+	$('main').html(appTemplates.getNewNoteTemp());
+}
+
+function renderNoteTemp(noteObj) {
+	$('main').html((appTemplates.getNoteTemp(noteObj.id, noteObj.title, noteFormattingServices.highlight(noteFormattingServices.noteFormatter(noteObj.content)))));
+}
+
+function renderEditTemp(noteObj) {
+	$('main').html(appTemplates.getEditTemp(noteObj.id, noteObj.title, noteObj.content));
+}
+
+//EVENT LISTENERS
+
+//start demo
+function getDemoHome() {
+	//return the json data..
+	$('main').on('click', '.get-demo', function(event) {
+		return appTemplates.getUserHome()
+	})
+
+	$('section').on('click', '.home', function(event) {
+		return appTemplates.getUserHome();
+	})
+}
+
+
+function demoNewNote() {
+	$('section').on('click', 'a.demo-new-note', function(event) {
+		event.preventDefault();
+		return renderNewNoteTemp(); 
+	});
+}
+
+
+function demoCreateNote() {
+	$('main').on('click', '.create-note', function(event) {
+		event.preventDefault();
+		$('.note-error-message').hide();
+		$('.highlighted-error').hide();
+		var newTitle = $('#new-title').val();
+		var newNoteText = $('#new-note').val();
+		var formattedNoteStr = noteFormattingServices.noteFormatter(newNoteText);
+		
+		if (newTitle.trim().length === 0 || newNoteText.trim().length === 0) {
+			return $('.note-error-message').show();
+		}
+		
+		if(!noteFormattingServices.highlight(formattedNoteStr)) { //this line might cause bug
+	 		return $('.highlighted-error').show()
+	 	}
+
+	 	state.dbIdSetter += 1
+	 	
+	 	var newNote = {
+	 		id: state.dbIdSetter,
+	 		user: 200,
+	 		title: newTitle,
+	 		content: newNoteText 
+	 	}
+
+	 	var userNoteRef = {
+	 		id: state.dbIdSetter,
+	 		title: newTitle,
+	 		content: newNoteText
+	 	}
+	 	
+	 	state.notes.push(newNote);
+	 	state.users[0].userNotes.push(userNoteRef);
+		
+		for (var i=0; i<state.notes.length; i++) {
+			if (state.notes[i].id === state.dbIdSetter) {
+				return renderNoteTemp(state.notes[i]);
+			}
+		}
+	
+	});
+}
+
+function demoEditNote() {
+	//clicking on both the edit note link as well as the div 
+	//in the actual note. 
+	$('main').on('click', 'a.edit', function(event) {
+		event.preventDefault();
+		var cardContentParent = $(this).closest('div.card-content'); //the parent 
+		var noteId = cardContentParent.find('div.note-id').text(); 
+		
+		for (var i=0; i<state.notes.length; i++) {
+			if (state.notes[i].id === parseInt(noteId)) {
+				return renderEditTemp(state.notes[i]);
+			}
+		}
+	})
+
+	$('main').on('click', 'div.note', function(event) {
+		event.preventDefault();
+		var noteText = $(this).html();
+		var removedHighlighterText = noteFormattingServices.removeHighlighter(noteText); //remove this for protecting textarea from nonsense
+		var editorText = noteFormattingServices.editorFormatter(removedHighlighterText);//FOR THE text area! //TODO BETTER IMPLEMENTATION!
+		$('#edit-note').val(editorText);
+		$('div.editing-note-container').show();
+		$('div.note').hide();  
+	})
+
+}
+
+function demoUpdateNote() {
+	$('main').on('click', 'button.save-note', function(event) {
+		event.preventDefault();
+		$('.note-error-message').hide();
+		$('.highlighted-error').hide()
+		var noteText = $('textarea').val();
+		var noteId = $('div.note-id').text();
+		var formattedNoteStr = noteFormattingServices.noteFormatter(noteText); //FOR THE NOTE DIV //TODO BETTER IMPLEMENTATION!
+		if (noteText.trim().length === 0) {
+			return $('.note-error-message').show();
+		}	
+
+	 	if(!noteFormattingServices.highlight(formattedNoteStr)) {
+	 		return $('.highlighted-error').show()
+	 	}
+
+		$('div.editing-note-container').hide();
+		$('div.note').html(noteFormattingServices.highlight(formattedNoteStr)).show();
+		
+		for (var i=0; i<state.notes.length; i++) {
+			if (state.notes[i].id === parseInt(noteId)) {
+				state.notes[i].content = noteText;
+				state.users[0].userNotes[i].content = noteText;
+			}
+		}	
+	
+		return console.log('updated successfully');
+	})
+}
+
+function demoEditTitle() {
+	$('main').on('click', '.note-title', function(event) {
+		event.preventDefault();  
+		var noteTitle = $(this).text();
+		$('div.note-title').hide();
+		$('#edit-title').val(noteTitle);
+		$('div.edit-title-container').show(); 
+	})
+}
+
+function demoCancelTitleUpdate() {
+	$('main').on('click', '#cancel-update', function(event) {
+		event.preventDefault();
+		$('.edit-title-container').hide();
+		$('.note-title').show();
+	})
+}
+
+function demoUpdateTitle() {
+	//updating the titles
+	$('main').on('click', '#update-title', function(event) {
+		event.preventDefault();
+		$('.emtpy-titles-error').hide();
+		var noteId = $('div.note-id').text();
+
+		var newTitle = $('#edit-title').val();
+		//client side validation //test this!
+		if (newTitle.trim() === 0) {
+			return $('.emtpy-titles-error').show();;
+		}
+
+		for (var i=0; i<state.notes.length; i++) {
+			if (state.notes[i].id === parseInt(noteId)) {
+				state.notes[i].title = newTitle;
+				state.users[0].userNotes[i].title = newTitle;
+			}
+		}
+	})
+}
+
+function demoDeleteNote() {
+	$('main').on('click', '.delete', function(event) {
+		event.preventDefault();
+		$(this).parent().next().show(); //traverse up and then down to the next p element containing the confirmation
+	})
+}
+
+function demoConfirmDelete() {
+	$('main').on('click', '.confirm-delete', function(event) {
+		event.preventDefault();
+		var noteId = $(this).closest('div.card-content').find('div.note-id').text();
+		var userNoteContainer = $(this).closest('.user-note').hide(); 
+	 	
+	 	for (var i=0; i<state.notes.length; i++) {
+			if (state.notes[i].id === parseInt(noteId)) {
+				state.notes.splice(i, 1)
+				state.users[0].userNotes.splice(i, 1)
+			}
+		}
+	})	
+}
+
+function hideTip() {
+	$('main').on('click', '.hide-tip', function(event) {
+		event.preventDefault();
+		$('.tips').hide();
+	})
+}
+
 function renderLoginMarkup() {
 	$('main').on('click', '.get-login', function(event) {
 		event.preventDefault();
@@ -13,8 +262,18 @@ function renderSignupMarkup() {
 		return $('main').html(getSignupTemplate());
 	})	
 }
-
 $(function() {
+	getDemoHome();
+	demoNewNote();
+	demoCreateNote();
+	demoEditNote();
+	demoUpdateNote();
+	demoEditTitle();
+	demoCancelTitleUpdate();
+	demoUpdateTitle();
+	demoDeleteNote();
+	demoConfirmDelete();
+	hideTip();
 	renderLoginMarkup();
 	renderSignupMarkup();
 })
---
+++ public/js/unauthorizedServices.js
@@ -0,0 +1,284 @@
+function getLoginTemplate() {
+    return `<div class="container login">
+                <div class="row">
+                    <div class="col s12 m6 offset-m3 app-logo">
+                        <h2 class="app-name authentication-page-header">Slick Notes</h2>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col s12 m6 offset-m3 login-content">
+                            <form method="post" action="/login" class="login-form">
+                                <fieldset name="login-form">
+                                    <label class="login-label">Username</label>
+                                    <input type="text" name="username" class="login-field" placeholder="johnsmith28" required />
+                                    <label class="login-label">Password</label>
+                                    <input type="password" name="password" class="login-field" required />
+                                    <button type="submit" class="authentication-button login">Login</button>
+                                </fieldset>
+                            </form>
+                            <a href="#" class="signup-login-links-container get-signup">Sign Up</a>
+                    </div>
+                </div>
+            </div>`
+}
+
+function getSignupTemplate() {
+    return `<div class="container signup">
+                <div class="row">
+                    <div class="col s12 m6 offset-m3 app-logo">
+                        <h2 class="app-name authentication-page-header">Slick Notes</h2>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col s12 m6 offset-m3 signup-content">
+                        <form method="post" action="/users" class="signup-form">
+                            <fieldset name="signup-form">
+                                <label class="signup-label">Email</label>
+                                <input type="text" name="email" class="signup-field" placeholder="foo@bar.com" required />
+                                <label class="signup-label">Username</label>
+                                <input type="text" name="username" class="signup-field" placeholder="johnsmith123" required />
+                                <label class="signup-label">Password</label>
+                                <input type="password" name="password" class="signup-field" required />
+                                <label class="signup-label">Confirm Password</label>
+                                <input type="password" name="passwordConfirmation" class="signup-field" required />
+                                <button class="authentication-button signup" type="submit">Sign Up</button>
+                            </fieldset>
+                        </form>
+                        <a href="#" class="signup-login-links-container get-login">Login</a>
+                    </div>
+                </div>
+            </div>`
+}
+
+var noteFormattingServices = {
+    noteFormatter: function (string) {
+        var lineBreak = '<br>';
+        var brString = string.replace(/\n/g, lineBreak);
+        function spaceRecognizer(brString) {
+            var nbSpace = '&nbsp;';
+            var fullyFormattedStr = brString.replace(/\s/g, nbSpace);
+            return fullyFormattedStr;
+        }
+        return spaceRecognizer(brString);
+    },
+
+    editorFormatter: function (string) {
+        var lineBreak = '\n';
+        var brString = string.replace(/<br>/g, lineBreak);
+        function spaceRecognizer(brString) {
+            var editorSpace = ' ';
+            var formattedEditor = brString.replace(/&nbsp;/g, editorSpace);
+            return formattedEditor;
+        }
+        return spaceRecognizer(brString);
+    },
+
+    highlight: function (string) {
+        //add yellow highlighting when two tick marks are encloessed by anothed
+        //two tick marks
+        $('div.highlighted-error').hide();
+        var stringCharacters = string.split('');
+        var tick = '`';
+        var tickCounter = 0;
+        for (var i=0, length=stringCharacters.length; i<length; i++) {
+            var currentCharacter = stringCharacters[i];
+            var nextCharacter = stringCharacters[i+1];
+            if (currentCharacter===tick && nextCharacter===tick) {
+                tickCounter += 1;
+                if (tickCounter % 2 !== 0) {
+                    stringCharacters.splice(i, 1, '<span class="highlighted">')
+                    stringCharacters.splice(i+1, 1);
+                }
+                else { 
+                    stringCharacters.splice(i, 1, '</span>')
+                    stringCharacters.splice(i+1, 1);
+                } 
+            }
+        }
+    
+        if (tickCounter % 2 !== 0) {
+            return false
+        }
+        return stringCharacters.join('');
+    },
+
+    removeHighlighter: function (string) {
+        var removeOpeningTags = string.replace(/<span class="highlighted">/g, '``');
+        function removeClosingTags(string) {
+            var textString = string.replace(/<\/span>/g, '``');
+            return textString;
+        }
+        return removeClosingTags(removeOpeningTags);
+    }
+
+}
+
+var appTemplates = {
+
+    ticks: '``',
+
+    getUserHome: function () {
+        event.preventDefault();
+        //get mock data in json like format... simulating when real user logs in.
+        var notes = state.users[0].userNotes;
+        var userHomeHTML = '';
+        notes.forEach(function (note) {
+            userHomeHTML  += appTemplates.createNoteFeed(note);
+        })
+        $('section').html(appTemplates.getNavTemp());
+        $('main').html('<div class="container">' + userHomeHTML + '</div>');
+        window.setTimeout(function() {
+          $(".button-collapse").sideNav(); 
+            $('.parallax').parallax();
+            $(".dropdown-button").dropdown({
+                hover: false
+            })  
+        }, 0)
+    },
+
+    getNewNoteTemp: function () {
+            return  `<div class="note-container container">    
+                        <div class="row">    
+                            <div class="col s12 m10 offset-m1">
+                                <div class="edit-title-container create-note-title">
+                                    <label class="new-note-label">Title:</label>
+                                    <input type="text" id="new-title" placeholder="ex. Today's Class" />
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col s12 m10 offset-m1">
+                                <div class="editing-note-container create-note-content">
+                                    <div class="tips">
+                                        Tip: Hightlight word with backticks. ${this.ticks}highlighted word${this.ticks}
+                                        <a href="#" class="hide-tip"><i class="material-icons right">done</i></a>
+                                    </div>
+                                    <div class="note-error-message">PLEASE MAKE SURE NOTE TO LEAVE A BLANK NOTE!</div>
+                                    <div class="highlighted-error">PLEASE MAKE SURE ALL TICKS ARE INCLUDED IF HIGHLIGHTING.</div>
+                                    <label class="new-note-label">Note:</label>
+                                    <textarea id="new-note" class="note-editor" placeholder="ex. Today was fun. I learned nothing."></textarea>
+                                    <div class="create-button-container"><button class="create-note">Create</button></div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>`
+    },
+
+    //The next two functions should be refactored 
+    getNoteTemp: function (noteId, title, content) {
+        return `<div class="note-id">${noteId}</div>
+                <div class="container note-container">
+                    <div class="row">
+                        <div class="col s12 m10 offset-m1">
+                            <div class="note-title">${title}</div>    
+                            <div class="edit-title-container" style="display: none;">
+                                <input type="text" id="edit-title" />
+                                <button type="submit" id="update-title">Update</button>
+                                <button id="cancel-update">Cancel</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col s12 m10 offset-m1">
+                            <div class="note hide-note">${content}</div>
+                            <div class="editing-note-container" style="display: none;">
+                                <div class="tips">
+                                    Tips: Hightlight using backticks: ${this.ticks}highlighted word${this.ticks}
+                                    <a href="#" class="hide-tip"><i class="material-icons right">done</i></a>
+                                </div>
+                                <div class="note-error-message">PLEASE MAKE SURE NOTE TO LEAVE A BLANK NOTE!</div>
+                                <div class="highlighted-error">PLEASE MAKE SURE ALL TICKS ARE INCLUDED IF HIGHLIGHTING.</div>
+                                <textarea id="edit-note" class="note-editor"></textarea>
+                                <div class="save-button-container"><button class="save-note">Save</button></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>`
+    },
+
+    getEditTemp: function (noteId, title, content) {
+        return `<div class="note-id">${noteId}</div>
+                <div class="container note-container">
+                    <div class="row">
+                        <div class="col s12 m10 offset-m1">
+                            <div class="note-title">${title}</div>    
+                            <div class="edit-title-container" style="display: none;">
+                                <input type="text" id="edit-title" />
+                                <button type="submit" id="update-title" class="update-title-button">Update</button>
+                                <button id="cancel-update" class="update-title-button">Cancel</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col s12 m10 offset-m1">
+                            <div class="note hide-note"></div>
+                            <div class="editing-note-container">
+                                <div class="tips">
+                                    Tip: Hightlight word with backticks. ${this.ticks}highlighted word${this.ticks}
+                                    <a href="#" class="hide-tip"><i class="material-icons right">done</i></a>
+                                </div>  
+                                <div class="note-error-message">PLEASE MAKE SURE NOTE TO LEAVE A BLANK NOTE!</div>
+                                <div class="highlighted-error">PLEASE MAKE SURE ALL TICKS ARE INCLUDED IF HIGHLIGHTING.</div>
+                                <textarea id="edit-note" class="note-editor">${content}</textarea>
+                                <div class="save-button-container"><button class="save-note">Save</button></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>`
+    },
+
+    getNavTemp: function() {
+        return `<nav><a href="#" data-activates="slide-out" class="menu button-collapse"><i class="material-icons">reorder</i></a>
+                    <ul id="slide-out" class="side-nav">
+                        <li>
+                            <a href="#!" class="home demo-home nav-button">Home</a>
+                        </li>
+                        <li>
+                            <a href="#" class="new-note-button demo-new-note nav-button">New Note</a>
+                        </li>
+                        <li>
+                            <div class="divider"></div>
+                        </li>
+                    </ul>
+
+                    <a hre="#" class="home get-demo nav-button mobile-logo center">Slick Note</a>
+                    <ul class="medium-viewport-nav right">
+                        <li class="medium-view nav-button">
+                            <a href="#!" class="home demo-home nav-button">Home</a>
+                        </li>
+                        <li class="medium-view nav-button">
+                            <a href="#!" class="new-note-button demo-new-note nav-button">New Note</a>
+                        </li>
+                    </ul></nav> `
+    },
+
+    createNoteFeed: function(note) {
+    let content = noteFormattingServices.highlight(note.content);
+        return  `<div class="row">
+            <div class="col s12 m10 offset-m1">
+            <div class="card user-note">
+                <div class="card-content">
+                    <div class="note-id" style="display: none;">${note.id}</div>
+                    <div class="row">
+                        <div class="col s10">
+                            <span class="card-title grey-text text-darken-4">${note.title}</span>
+                        </div>
+                        <div class="col s2">
+                            <span class="activator"><i class="material-icons right">more_vert</i></span>
+                        </div>
+                    </div>
+                    <p class="card-links">
+                        <a href="#" class="edit"><i class="material-icons link-options">mode_edit</i></a>
+                        <a href="#" class="delete"><i class="material-icons link-options">delete</i></a>
+                    </p>
+                    <p class="deletion-question" style="display: none;">Are You Sure?<a href="#" class="confirm-delete">Yes</a></p>
+                </div>
+                <div class="card-reveal">
+                    <span class="card-title grey-text text-darken-4">${note.title}<i class="material-icons right">close</i></span>
+                    <p>${content}</p>
+                </div>
+            </div>
+            </div>
+            </div>`
+    }
+}
